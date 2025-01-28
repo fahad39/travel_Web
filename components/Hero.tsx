626df4842261745a7ad5511f36138d4c8355d456
fahad39 <fahadhussain0127@gmail.com>
2025-01-29 00:04:46 +0500

    added code

---
+++ components/Hero.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hero = () => {
+  return <section className="max-container pladdingCounter flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-12"
+};
+
+export default Hero;
